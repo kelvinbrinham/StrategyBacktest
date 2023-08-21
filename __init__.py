b40b8bbf5371837fbea847d7f68b0621d6c4866d
Kelvin Brinham <kelvin.brinham@richfox.com>
2023-08-21 17:35:57 +0100

    Delete root init.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""Strategy Backtest"""
-
-from .strategybacktest.backtest import Backtest, BacktestAnalysis  # noqa: F401
-from .strategybacktest.functions import (  # noqa: F401
-    data_collector,
-    excel_summary_2_latex,
-)
-from .strategybacktest.portfolio import Portfolio  # noqa: F401
-from .strategybacktest.strategy import DummyStrategy, Strategy  # noqa: F401
