762d4e460dd64b5e523d2aab409fbb139be4f735
Kelvin Brinham <kelvin.brinham@richfox.com>
2023-08-21 17:42:13 +0100

    Write __init__ file

--- src/strategybacktest/__init__.py
+++ src/strategybacktest/__init__.py
@@ -1,1 +1,6 @@
 """Strategy Backtest"""
+
+from .backtest import Backtest, BacktestAnalysis
+from .functions import data_collector, excel_summary_2_latex
+from .portfolio import Portfolio
+from .strategy import DummyStrategy, Strategy
