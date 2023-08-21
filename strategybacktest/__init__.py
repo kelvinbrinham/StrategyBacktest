8e1546c3a64db99b89e601e57cb4ecb885a9bf12
Kelvin Brinham <kelvin.brinham@richfox.com>
2023-08-21 17:30:15 +0100

    Change file paths

--- strategybacktest/__init__.py
+++ strategybacktest/__init__.py
@@ -1,6 +1,1 @@
 """Strategy Backtest"""
-
-from backtest import Backtest, BacktestAnalysis  # noqa: F401
-from functions import data_collector, excel_summary_2_latex  # noqa: F401
-from portfolio import Portfolio  # noqa: F401
-from strategy import DummyStrategy, Strategy  # noqa: F401
