f95413a6db71645b88dd83c7c93792c3d55e4f93
Kelvin Brinham <kelvin.brinham@richfox.com>
2023-08-24 12:49:46 +0100

    Remove DummyStrategy from Strategy script.

--- src/strategybacktest/strategy.py
+++ src/strategybacktest/strategy.py
@@ -31,32 +31,3 @@ class Strategy(ABC):
             Dictionary of target weights.
         """
         pass
-
-
-class DummyStrategy(Strategy):
-    """
-    Dummy strategy class which takes predetermined weights from a dataframe.
-
-    Args:
-        weights_df: Time-series dataframe of weights for each asset.
-    """
-
-    def __init__(self, weights_df: pd.DataFrame) -> None:
-        self.weights_df = weights_df
-        self._current_weights = {}
-
-    def __call__(self, ts: pd.Timestamp, **kwargs) -> Dict[str, float]:
-        """
-        Rebalance portfolio according to predetermined weights for each new
-        timestamp.
-
-        Args:
-            ts: Timestamp for rebalance.
-
-        Returns:
-            Dictionary of target weights for each ticker.
-        """
-        if ts in self.weights_df.index:
-            self._current_weights = self.weights_df.loc[ts].to_dict()
-
-        return self._current_weights
